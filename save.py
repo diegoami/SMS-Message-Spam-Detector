e6c109dfaa95fef74bcf61c67da7ff71f526739a
Diego Amicabile <diego.amicabile@gmail.com>
2019-07-08 15:07:29 +0200

    Add app and save

--- save.py
+++ save.py
@@ -4,6 +4,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
 from sklearn.externals import joblib
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("spam.csv", encoding="latin-1")
 df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
@@ -13,15 +16,14 @@ X = df['message']
 y = df['label']
 
 # Extract Feature With CountVectorizer
-cv = CountVectorizer()
-X = cv.fit_transform(X)  # Fit the Data
-from sklearn.model_selection import train_test_split
+clf = Pipeline(memory=None,
+         steps=[('cv', CountVectorizer()),
+                ('mnb', MultinomialNB())], verbose=False)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 # Naive Bayes Classifier
-from sklearn.naive_bayes import MultinomialNB
 
-clf = MultinomialNB()
 clf.fit(X_train, y_train)
 print(clf.score(X_test, y_test))
 
